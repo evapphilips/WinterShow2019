4f096b91dae2c7888a39d711c66e613cac83c24b
evapphilips <ep1521@nyu.edu>
2019-12-13 12:07:08 -0500

    adding second display

--- secondaryDisplay/sketch.js
+++ secondaryDisplay/sketch.js
@@ -4,79 +4,81 @@ var height = window.innerHeight
 // access body
 var body = d3.select("body");
 
+
+////// SECTION 1 //////////
 // make section 1 div
 var section1 = body.append('div')
-    .attr("class", "section1")
+    .attr("class", "section")
 
 // add section 1 header
 section1.append('h1')
     .attr("class", "heading")
     .text("My browsing data this week")
 
-// add circles
-var circleContainer = section1.append('svg')
+// add circle container for the week
+var circleContainer1 = section1.append('svg')
     .attr("class", "circle-container")
     .attr("width", width)
     .attr("height", height/2)
 
-var maxSize = width/14;
-var yPos = width/7
+var maxSize1 = width/14;
+var yPos1 = width/7
 var opacity = "1.00";
-var weekData = [20, 60, 40, 28, 37, 50, 23];
-var maxWeekSearch = 50;
+var weekData = [314, 238, 231, 199, 285, 200, 200]
+var maxWeekSearch = 1000;
 
-// add all seven circles
-circleContainer.append("circle")
-    .attr("cx", maxSize)
-    .attr("cy", yPos)
-    .attr("r", (weekData[0] * maxSize)/maxWeekSearch)
+// add all seven circles for the week
+circleContainer1.append("circle")
+    .attr("cx", maxSize1)
+    .attr("cy", yPos1)
+    .attr("r", (weekData[0] * maxSize1)/maxWeekSearch)
     .attr("fill", "url(#radial-gradient)")
     .style("opacity", ".95")
 
-circleContainer.append("circle")
-    .attr("cx", 3*maxSize)
-    .attr("cy", yPos)
-    .attr("r", (weekData[1] * maxSize)/maxWeekSearch)
+circleContainer1.append("circle")
+    .attr("cx", 3*maxSize1)
+    .attr("cy", yPos1)
+    .attr("r", (weekData[1] * maxSize1)/maxWeekSearch)
     .attr("fill", "url(#radial-gradient)")
     .style("opacity", ".95")
 
-circleContainer.append("circle")
-    .attr("cx", 5*maxSize)
-    .attr("cy", yPos)
-    .attr("r", (weekData[2] * maxSize)/maxWeekSearch)
+circleContainer1.append("circle")
+    .attr("cx", 5*maxSize1)
+    .attr("cy", yPos1)
+    .attr("r", (weekData[2] * maxSize1)/maxWeekSearch)
     .attr("fill", "url(#radial-gradient)")
     .style("opacity", ".95")
 
-circleContainer.append("circle")
-    .attr("cx", 7*maxSize)
-    .attr("cy", yPos)
-    .attr("r", (weekData[3] * maxSize)/maxWeekSearch)
+circleContainer1.append("circle")
+    .attr("cx", 7*maxSize1)
+    .attr("cy", yPos1)
+    .attr("r", (weekData[3] * maxSize1)/maxWeekSearch)
     .attr("fill", "url(#radial-gradient)")
     .style("opacity", ".95")
 
-circleContainer.append("circle")
-    .attr("cx", 9*maxSize)
-    .attr("cy", yPos)
-    .attr("r", (weekData[4] * maxSize)/maxWeekSearch)
+circleContainer1.append("circle")
+    .attr("cx", 9*maxSize1)
+    .attr("cy", yPos1)
+    .attr("r", (weekData[4] * maxSize1)/maxWeekSearch)
     .attr("fill", "url(#radial-gradient)")
     .style("opacity", ".95")
 
-circleContainer.append("circle")
-    .attr("cx", 11*maxSize)
-    .attr("cy", yPos)
-    .attr("r", (weekData[5] * maxSize)/maxWeekSearch)
+circleContainer1.append("circle")
+    .attr("cx", 11*maxSize1)
+    .attr("cy", yPos1)
+    .attr("r", (weekData[5] * maxSize1)/maxWeekSearch)
     .attr("fill", "url(#radial-gradient)")
     .style("opacity", ".95")
 
-circleContainer.append("circle")
-    .attr("cx", 13*maxSize)
-    .attr("cy", yPos)
-    .attr("r", (weekData[6] * maxSize)/maxWeekSearch)
+circleContainer1.append("circle")
+    .attr("cx", 13*maxSize1)
+    .attr("cy", yPos1)
+    .attr("r", (weekData[6] * maxSize1)/maxWeekSearch)
     .attr("fill", "url(#radial-gradient)")
     .style("opacity", ".95")
 
 // define radial gradient
-var radialGradient = circleContainer.append("defs")
+var radialGradient = circleContainer1.append("defs")
   .append("radialGradient")
     .attr("id", "radial-gradient");
 
@@ -87,4 +89,51 @@ radialGradient.append("stop")
 radialGradient.append("stop")
     .attr("offset", "100%")
     .attr("stop-color", "#fff");
+
+
+
+
+////// SECTION 2 //////////
+// make section 2 div
+var section2 = body.append('div')
+    .attr("class", "section")
+
+// add section 2 header
+section2.append('h1')
+    .attr("class", "heading")
+    .text("Estimated Server Requests for Big Tech")
+
+// add circle container for the companies
+var circleContainer2 = section2.append('svg')
+    .attr("class", "circle-container")
+    .attr("width", width)
+    .attr("height", height/2)
+
+var maxSize2 = width/4;
+var yPos2 = width/4
+var techData = [63000, 70000];
+var maxTechSearch = 70000;
+
+// add all three circles for tech
+circleContainer2.append("circle")
+    .attr("cx", maxSize2)
+    .attr("cy", yPos2)
+    .attr("r", (techData[0] * maxSize2)/maxTechSearch)
+    .attr("fill", "url(#radial-gradient)")
+    .style("opacity", ".95")
+
+circleContainer2.append("circle")
+    .attr("cx", 3*maxSize2)
+    .attr("cy", yPos2)
+    .attr("r", (techData[1] * maxSize2)/maxTechSearch)
+    .attr("fill", "url(#radial-gradient)")
+    .style("opacity", ".95")
+
+circleContainer2.append("circle")
+    .attr("cx", 5*maxSize2)
+    .attr("cy", yPos2)
+    .attr("r", (techData[2] * maxSize2)/maxTechSearch)
+    .attr("fill", "url(#radial-gradient)")
+    .style("opacity", ".95")
+
    
